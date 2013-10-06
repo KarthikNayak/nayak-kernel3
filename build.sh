ad8e9333e256d71ac7ba786cf26a7a7d62e1ef00
Karthik Nayak <karthik.188@gmail.com>
2013-10-06 16:20:31 +0530

    My files

--- build.sh
+++ build.sh
@@ -12,6 +12,7 @@ location=.
 vendor=lge
 
 if [ -z $target ]; then
+read -p "What is the Version No? " version
 echo "choose your target device"
 echo "1) p350"
 echo "2) p500"
@@ -21,28 +22,17 @@ echo "5) p509"
 read -p "1/2/3/4/5: " choice
 case "$choice" in
 1 ) export target=p350 ; export defconfig=cyanogenmod_p350_defconfig;;
-2 ) export target=p500 ; export defconfig=cyanogenmod_p500_p509_defconfig;;
+2 ) export target=p500 ; export defconfig=cyanogenmod_p500_defconfig;;
 3 ) export target=p505 ; export defconfig=cyanogenmod_p505_p506_defconfig;;
 4 ) export target=p506 ; export defconfig=cyanogenmod_p505_p506_defconfig;;
-5 ) export target=p509 ; export defconfig=cyanogenmod_p500_p509_defconfig;;
+5 ) export target=p509 ; export defconfig=cyanogenmod_p509_defconfig;;
 * ) echo "invalid choice"; sleep 2 ; ./build.sh;;
 esac
 fi # [ -z $target ]
 
-if [ -z $compiler ]; then
-if [ -f ../arm-eabi-4.6/bin/arm-eabi-* ]; then
-export compiler=../arm-eabi-4.6/bin/arm-eabi-
-elif [ -f arm-eabi-4.6/bin/arm-eabi-* ]; then # [ -f ../arm-eabi-4.6/bin/arm-eabi-* ]
-export compiler=arm-eabi-4.6/bin/arm-eabi-
-else # [ -f arm-eabi-4.6/bin/arm-eabi-* ]
-echo "please specify a location, including the '/bin/arm-eabi-' at the end "
-read compiler
-fi # [ -z $compiler ]
-fi # [ -f ../arm-eabi-4.6/bin/arm-eabi-* ]
-
 cd $location
 export ARCH=arm
-export CROSS_COMPILE=$compiler
+export CROSS_COMPILE=../toolchains/linux-arm-linux-androideabi-4.7/bin/arm-linux-androideabi-
 if [ -z "$clean" ]; then
 read -p "do make clean mrproper?(y/n)" clean
 fi # [ -z "$clean" ]
@@ -55,6 +45,7 @@ esac
 echo "now building the kernel"
 
 make $defconfig
+make menuconfig
 make -j `cat /proc/cpuinfo | grep "^processor" | wc -l` "$@"
 
 ## the zip creation
@@ -73,14 +64,14 @@ cp arch/arm/boot/zImage zip-creator/kernel
 
 find . -name *.ko | xargs cp -a --target-directory=zip-creator/system/lib/modules/
 
-zipfile="lge-3.0.x-$target-$daytime.zip"
+zipfile="Nayak-Kernel-$target-v$version.zip"
 cd zip-creator
 rm -f *.zip
 zip -r $zipfile * -x *kernel/.gitignore*
 
 echo "zip saved to zip-creator/$zipfile"
-
+mv $zipfile ~/android/Kernel/
+echo "Moved the file to Kernel"
 else # [ -f arch/arm/boot/zImage ]
 echo "the build failed so a zip won't be created"
 fi # [ -f arch/arm/boot/zImage ]
-
---
+++ edit.sh
@@ -0,0 +1,2 @@
+echo "Editing"
+gedit arch/arm/configs/cyanogenmod_p500_p509_defconfig gedit arch/arm/configs/cyanogenmod_p505_p506_defconfig
